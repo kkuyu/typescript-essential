026524de7bf3d5609ac13ef556a8b97a45b36576
kkuyu <devils67@naver.com>
2022-01-27 19:50:27 +0900

    CH03_03. 두 개의 화면을 가진 웹앱

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const ajax = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+
+const container = document.getElementById("root");
+const content = document.createElement("div");
 
 ajax.open("GET", NEWS_URL, false);
 ajax.send();
@@ -7,10 +11,30 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement("ul");
 
+window.addEventListener("hashchange", function () {
+  const id = window.location.hash.substring(1);
+
+  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  const title = document.createElement("h1");
+
+  title.innerHTML = newsContent.title;
+  content.appendChild(title);
+  console.log(newsContent);
+});
+
 for (let i = 0; i < 10; i++) {
   const li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
+  const a = document.createElement("a");
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById("root").appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
