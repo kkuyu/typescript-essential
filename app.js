a650a2671926a03ea1ef9c90808f3f3d5b564c75
kkuyu <devils67@naver.com>
2022-01-28 18:12:40 +0900

    CH03_04. 문자열을 활용한 HTML 다루기

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 const container = document.getElementById("root");
 const content = document.createElement("div");
 
-ajax.open("GET", NEWS_URL, false);
-ajax.send();
+function getData(url) {
+  ajax.open("GET", url, false);
+  ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement("ul");
 
 window.addEventListener("hashchange", function () {
   const id = window.location.hash.substring(1);
 
-  ajax.open("GET", CONTENT_URL.replace("@id", id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
   const title = document.createElement("h1");
 
   title.innerHTML = newsContent.title;
@@ -26,14 +27,13 @@ window.addEventListener("hashchange", function () {
 });
 
 for (let i = 0; i < 10; i++) {
-  const li = document.createElement("li");
-  const a = document.createElement("a");
+  const div = document.createElement("div");
 
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title}(${newsFeed[i].comments_count})`;
+  div.innerHTML = `<li>
+    <a href="#${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
+  </li>`;
 
-  li.appendChild(a);
-  ul.appendChild(li);
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
