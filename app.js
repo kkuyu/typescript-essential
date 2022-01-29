409c2e26deeb4db5641c654b79344e4cb1cba737
kkuyu <devils67@naver.com>
2022-01-29 20:21:00 +0900

    CH03_05. 라우터？ 화면 처리기 만들기

--- app.js
+++ app.js
@@ -12,29 +12,46 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement("ul");
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener("hashchange", function () {
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`<li>
+      <a href="#${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
+    </li>`);
+  }
+
+  newsList.push("</ul>");
+
+  container.innerHTML = newsList.join("");
+}
+
+function newsDetail() {
   const id = window.location.hash.substring(1);
 
   const newsContent = getData(CONTENT_URL.replace("@id", id));
-  const title = document.createElement("h1");
 
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
-  console.log(newsContent);
-});
-
-for (let i = 0; i < 10; i++) {
-  const div = document.createElement("div");
-
-  div.innerHTML = `<li>
-    <a href="#${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a>
-  </li>`;
-
-  ul.appendChild(div.firstElementChild);
+  container.innerHTML = `<div>
+      <h1>${newsContent.title}</h1>
+      <div>
+        <a href="#">목록으로</a>
+      </div>
+    </div>`;
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function router() {
+  const routePath = window.location.hash;
+
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener("hashchange", router);
+
+router();
