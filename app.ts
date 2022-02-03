1affa43e12ab94f8cd242b4281406025e2896218
kkuyu <devils67@naver.com>
2022-02-03 21:00:14 +0900

    CH04_02. 변수에 타입 작성하기

--- app.ts
+++ app.ts
@@ -1,13 +1,29 @@
-const ajax = new XMLHttpRequest();
+type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+};
+
+type NewsFeed = {
+  id: number;
+  comments_count: number;
+  url: string;
+  user: string;
+  time_ago: string;
+  points: number;
+  title: string;
+  read?: boolean;
+};
+
+const ajax: XMLHttpRequest = new XMLHttpRequest();
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
 
-const store = {
+const store: Store = {
   currentPage: 1,
   feeds: [],
 };
 
-const container = document.getElementById("root");
+const container: HTMLElement | null = document.getElementById("root");
 
 function getData(url) {
   ajax.open("GET", url, false);
@@ -24,8 +40,16 @@ function makeFeeds(feeds) {
   return feeds;
 }
 
+function updateView(html) {
+  if (container) {
+    container.innerHTML = template;
+  } else {
+    console.error("최상위 컨테이너가 없어 UI를 진행할 수 없습니다.");
+  }
+}
+
 function newsFeed() {
-  let newsFeed = store.feeds;
+  let newsFeed: NewsFeed[] = store.feeds;
   const newsList = [];
 
   let template = `<div class="bg-gray-600 min-h-screen">
@@ -73,7 +97,7 @@ function newsFeed() {
   template = template.replace("{{__prev_page__}}", store.currentPage > 1 ? store.currentPage - 1 : 1);
   template = template.replace("{{__next_page__}}", store.currentPage < newsFeed.length / 10 ? store.currentPage + 1 : newsFeed.length / 10);
 
-  container.innerHTML = template;
+  updateView(template);
 }
 
 function newsDetail() {
@@ -129,7 +153,7 @@ function newsDetail() {
     return commentString.join("");
   }
 
-  container.innerHTML = template.replace("{{__comments__}}", makeComment(newsContent.comments));
+  updateView(template.replace("{{__comments__}}", makeComment(newsContent.comments)));
 }
 
 function router() {
