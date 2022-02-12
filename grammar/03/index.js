ed6769a6a540e6071cb2a37c26cb73146c9178b2
kkuyu <devils67@naver.com>
2022-02-12 20:35:27 +0900

    CH06_03. 문법-타입

---
+++ grammar/03/index.js
@@ -0,0 +1,20 @@
+function addAge(age) {
+  return age + 1;
+}
+
+let age = addAge(30);
+// let age = addAge('30'); 문자열로 변경한다
+
+console.log(age);
+
+age = 10;
+age = false;
+age = {};
+
+// function addAge(age){
+//     if(typeof age === 'number'){
+//         return age + 1;
+//     } else {
+//         😨
+//     }
+// }
