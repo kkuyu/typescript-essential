cfa90e80da5759aeece6be8a685c95431bec8055
kkuyu <devils67@naver.com>
2022-02-14 19:26:02 +0900

    CH06_09. 문법-예외

---
+++ grammar/09/index.js
@@ -0,0 +1,28 @@
+function doException() {
+  throw new Error("new Error");
+}
+function noException() {
+  return true;
+}
+
+function callException(type) {
+  if (type === "do") {
+    doException();
+  } else {
+    if (noException() === false) {
+      console.log("오류처리");
+    }
+  }
+}
+
+function main() {
+  try {
+    callException("do");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    console.log("done");
+  }
+}
+
+main();
