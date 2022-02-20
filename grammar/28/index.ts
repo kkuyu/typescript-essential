3bce610c8a708aa126a25675ff1b5e4e7e31c2c3
kkuyu <devils67@naver.com>
2022-02-20 18:16:35 +0900

    CH06_28. 문법-@types

---
+++ grammar/28/index.ts
@@ -0,0 +1,15 @@
+import { v4 } from "uuid";
+
+type UniqObject = {
+  id: string;
+  [key: string]: string | number | boolean;
+};
+
+const makeObject = (): UniqObject => ({
+  id: v4(),
+});
+
+console.log(makeObject());
+console.log(makeObject());
+
+// npm install @types/uuid
