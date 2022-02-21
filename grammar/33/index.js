b25258afbbcf174ffb1e6b9e13093845d3f22f94
kkuyu <devils67@naver.com>
2022-02-21 18:53:48 +0900

    CH06_33. 개념과 구조-이벤트 시스템

---
+++ grammar/33/index.js
@@ -0,0 +1,15 @@
+function main() {
+  const BUBBLING_PHASE = false;
+  const CAPTURING_PHASE = true;
+  const PHASE_NAME = ["NONE", "CAPTURING", "TARGET", "BUBBLING"];
+
+  function eventLogger(e) {
+    const { target, currentTarget, eventPhase } = e;
+    console.log(`${target.dataset.name}, ${currentTarget.dataset.name}, ${PHASE_NAME[eventPhase]}`);
+  }
+
+  let divs = document.querySelectorAll("div");
+  divs.forEach((div) => div.addEventListener("click", eventLogger, BUBBLING_PHASE));
+}
+
+document.addEventListener("DOMContentLoaded", main);
