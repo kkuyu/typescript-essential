d576e3d2563d48cdd1ac44e0e3fb5ef204afb6d9
kkuyu <devils67@naver.com>
2022-02-28 19:02:55 +0900

    CH06_54. 다음 세대의 nodejs & deno

---
+++ grammar/54/index.js
@@ -0,0 +1,4 @@
+// iwr https://deno.land/x/install/install.ps1 -useb | iex
+// curl -fsSL https://deno.land/x/install/install.sh | sh
+
+// deno run https://deno.land/std@0.120.0/examples/welcome.ts
