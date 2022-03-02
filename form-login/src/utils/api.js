08da0d06c451d51b51c5115e5bdfac1832064cb6
kkuyu <devils67@naver.com>
2022-03-02 20:40:45 +0900

    CH05_04~05_06. 로그인 폼

---
+++ form-login/src/utils/api.js
@@ -0,0 +1,18 @@
+export function get(url) {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export function post(url, data) {
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+}
